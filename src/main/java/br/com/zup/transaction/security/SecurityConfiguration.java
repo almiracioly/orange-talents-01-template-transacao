d8bf090a06c6ff421dc98b48ae9507edd35a4ec4
almiracioly <almir.acioly@gmail.com>
2021-03-02 01:33:30 -0300

    Implementacão de OAuth2

---
+++ src/main/java/br/com/zup/transaction/security/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package br.com.zup.transaction.security;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+
+@KeycloakConfiguration
+@Import({org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver.class})
+public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    @Bean
+    @Override
+    protected NullAuthenticatedSessionStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
